2b2b2ddd5914bc4e8d7ba635d3cb9f7ceb83a646
Siddiha <fathimasiddika62@gmail.com>
2026-05-12 11:14:46 +0530

    practiced some go

--- Go/Practice.go
+++ Go/Practice.go
@@ -1,40 +1,42 @@
+package main
+
 import "fmt"
 
-func add(a int, b int) int  {
+func add(a int, b int) int {
 	return a + b
 }
 
-func main(){
+func main() {
 	var name string
 	var age int
 
-	fmt.print("Enter your name: ")
-	fmt.scaln(&name)
+	fmt.Print("Enter your name: ")
+	fmt.Scanln(&name)
 
-	fmt.print("Enter your age: ")
-	fmt.scanln(&age)
+	fmt.Print("Enter your age: ")
+	fmt.Scanln(&age)
 
 	if age >= 18 {
-		fmt.println("hello", name, "you are an adult")
-	}else{
-		fmt.println("Hello", name, "you area minor")
+		fmt.Println("Hello", name, "you are an adult")
+	} else {
+		fmt.Println("Hello", name, "you are a minor")
 	}
 
 	//slice
 	numbers := []int{1, 2, 3, 4, 5}
-	fmt.println("/Number in slice:")
+	fmt.Println("Numbers in slice:")
 
 	//Loop
 	for _, num := range numbers {
-		fmt.println(num)
+		fmt.Println(num)
 	}
 
 	//function call
 	result := add(10, 5)
-	fmt.println("10 + 5=", result)
-	 fmt.Println("\nCountdown:")
+	fmt.Println("10 + 5 =", result)
+	fmt.Println("\nCountdown:")
 
-	 for i := 5 ; i >= 1 ; i--{
-		fmt.println(i)
-	 }
+	for i := 5; i >= 1; i-- {
+		fmt.Println(i)
+	}
 }
